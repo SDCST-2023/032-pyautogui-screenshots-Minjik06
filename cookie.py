d21c19f174dfe427ee61aef7e7503c37a584fe20
Minjik06 <minjik525@deltalearns.ca>
2023-05-04 11:35:14 -0700

    hjy

--- cookie.py
+++ cookie.py
@@ -1,22 +1,24 @@
+from ctypes.wintypes import RGB
 import pyautogui as p
 def cookieClick():
-    location = p.locateOnScreen('assets/cook.png', confidence = 0.8)
+    location = p.locateOnScreen('assets/cok.png', confidence = 0.8)
     if location!=None:
         p.moveTo(location)
         while location!=None:
             p.click()
-    return
+            if p.pixelMatchesColor(1237,296(102 ,255, 102)) or p.pixelMatchesColor(1237,296(97 ,231, 96)) or p.pixelMatchesColor(1237,296(97 ,241, 97)) or p.pixelMatchesColor(1237,296(112 ,218, 109)) or p.pixelMatchesColor(1237,296(98 ,242, 255)) or p.pixelMatchesColor(1237,291(97 ,231, 96)) or p.pixelMatchesColor(1237,291(97 ,241, 97)) or p.pixelMatchesColor(1237,291(112 ,218, 109)) or p.pixelMatchesColor(1237,291(98 ,242, 255)):
+                autocursor()
+                
+
+            
     
 def autocursor():
-    location = ('assets/cursor.png')
-    location1 = ('assets/cursor1.png')
-    if location1!=location:
-        a=int(input("y or n"))
-        if a=="y":
-            p.moveTo(location)
-            p.click()
-        elif a=="n":
-            cookieClick()
+    location = p.locateOnScreen('assets/cur.png')
+    p.moveTo(location)
+    p.click()
+    cookieClick()
+
+
 
 def main():
     cookieClick()
@@ -24,3 +26,18 @@ def main():
 
 
 main()
+
+"""cookie=0
+    green = p.pixelMatchesColor(1234,294)
+    location = p.locateOnScreen('assets/cok.png', confidence = 0.8)
+    if location!=None:
+        p.moveTo(location)
+        while location!=None:
+            p.click()
+            cookie+=click
+            if :
+                a = p.comfirm("Do you want to buy the cursor?")
+                if a:
+                    autocursor()
+                    numCur+=1
+                    curs=curs"""
