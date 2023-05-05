e1f6f3043d6cc3c8a3d69d2497fb304843fcded7
Minjik06 <minjik525@deltalearns.ca>
2023-05-05 11:35:25 -0700

    efsdf

--- cookie.py
+++ cookie.py
@@ -1,24 +1,41 @@
 from ctypes.wintypes import RGB
+import random
+import time
 import pyautogui as p
 def cookieClick():
-    location = p.locateOnScreen('assets/cok.png', confidence = 0.8)
-    if location!=None:
+    k=[autocursor(), Grandmother(), Farm()]
+    location = p.locateOnScreen('assets/cok.png')
+    p.moveTo(location)
+    while True: 
         p.moveTo(location)
-        while location!=None:
-            p.click()
-            if p.pixelMatchesColor(1237,296(102 ,255, 102)) or p.pixelMatchesColor(1237,296(97 ,231, 96)) or p.pixelMatchesColor(1237,296(97 ,241, 97)) or p.pixelMatchesColor(1237,296(112 ,218, 109)) or p.pixelMatchesColor(1237,296(98 ,242, 255)) or p.pixelMatchesColor(1237,291(97 ,231, 96)) or p.pixelMatchesColor(1237,291(97 ,241, 97)) or p.pixelMatchesColor(1237,291(112 ,218, 109)) or p.pixelMatchesColor(1237,291(98 ,242, 255)):
-                autocursor()
-                
-
+        if p.pixelMatchesColor(1218,333,(255,255,255)) and p.pixelMatchesColor(1218,277,(255,255,255)) and p.pixelMatchesColor(1220,391,(255,255,255)): 
+            num=random.randint(0,2)
+            k[num]
+        p.click(location)
+        if p.pixelMatchesColor(1220,391,(255,255,255)):
+                Farm()
+        p.click(location)
+        if p.pixelMatchesColor(1218,333,(255,255,255)):
+            Grandmother()
+        p.click(location)
+        if p.pixelMatchesColor(1218,277,(255,255,255)):
+            autocursor()
+        p.click(location)
             
     
 def autocursor():
-    location = p.locateOnScreen('assets/cur.png')
-    p.moveTo(location)
-    p.click()
-    cookieClick()
+    location1 = p.locateOnScreen('assets/cur.png')
+    p.click(location1)
+    
 
+def Grandmother():
+    location2 = p.locateOnScreen('assets/gra.png')
+    p.click(location2)
+    
 
+def Farm():
+    location3 = p.locateOnScreen('assets/far.png')
+    p.click(location3)
 
 def main():
     cookieClick()
@@ -40,4 +57,17 @@ main()
                 if a:
                     autocursor()
                     numCur+=1
-                    curs=curs"""
+                    curs=curs
+                    
+                    
+                    
+                    def cookieClick():
+    time.sleep(2)
+    location = p.locateOnScreen('assets/cok.png', confidence = 0.8)
+    if location!=None:
+        p.moveTo(location)
+        while location!=None:
+            if p.pixelMatchesColor(1218,325,(255,255,255)):
+                p.confirm("want to by cursor?")
+                autocursor()
+            p.click()"""
