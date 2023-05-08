4d61a9e06100f1aae81f7f898aa0cd6f0183bdd3
Minjik06 <minjik525@deltalearns.ca>
2023-05-08 11:35:40 -0700

    fsdfsdf

--- cookie.py
+++ cookie.py
@@ -2,7 +2,73 @@ from ctypes.wintypes import RGB
 import random
 import time
 import pyautogui as p
+from pyscreeze import pixel, pixelMatchesColor
+debug = False
+p.Pause = 0.001
+
+
 def cookieClick():
+    location = p.locateOnScreen('assets/cok.png')
+    r,g,b=p.pixel(1194,168)
+    print(r,g,b)
+    p.moveTo(location)
+    while True: 
+        for i in range(20):
+            p.click()
+        #print(p.pixel(1220,391), p.pixel(1218,333) , p.pixel(1218,277))
+            if p.pixelMatchesColor(1220,391,(255,255,255)):
+                Farm()
+                p.moveTo(location)
+            if p.pixelMatchesColor(1218,333,(255,255,255)):
+                Grandmother()
+                p.moveTo(location)
+            if p.pixelMatchesColor(1218,277,(255,255,255)):
+                autocursor()
+                p.moveTo(location)
+        for k in range(40):
+            if p.pixelMatchesColor(1194,168,(6,19,27)):
+                Store()
+                p.moveTo(location)
+    
+def autocursor():
+    if debug: print("autocursor")
+    location1 = p.locateOnScreen('assets/cur.png',confidence=0.9)
+    while p.pixelMatchesColor(1218,277,(255,255,255)):
+        p.click(location1)
+    
+
+def Grandmother():
+    if debug: print("grandmother")
+    location2 = p.locateOnScreen('assets/gra.png',confidence = 0.9)
+    """mp = p.position()
+    p.moveTo(location2)
+    time.sleep(0.25)
+    p.moveTo(mp)
+    """
+    while p.pixelMatchesColor(1218,333,(255,255,255)):
+        p.click(location2)
+    
+
+def Farm():
+    if debug: print("farm")
+    location3 = p.locateOnScreen('assets/far.png',confidence=0.9)
+    while p.pixelMatchesColor(1220,391,(255,255,255)):
+        p.click(location3)
+
+def Store():
+    if debug: print("Store")
+    location4 = p.locateOnScreen('assets/str.png',confidence=0.9)
+    p.click(location4)
+
+
+def main():
+    cookieClick()
+    
+
+
+main()
+
+"""def cookieClick():
     k=[autocursor(), Grandmother(), Farm()]
     location = p.locateOnScreen('assets/cok.png')
     p.moveTo(location)
@@ -21,30 +87,29 @@ def cookieClick():
         if p.pixelMatchesColor(1218,277,(255,255,255)):
             autocursor()
         p.click(location)
-            
-    
-def autocursor():
-    location1 = p.locateOnScreen('assets/cur.png')
-    p.click(location1)
-    
-
-def Grandmother():
-    location2 = p.locateOnScreen('assets/gra.png')
-    p.click(location2)
-    
-
-def Farm():
-    location3 = p.locateOnScreen('assets/far.png')
-    p.click(location3)
-
-def main():
-    cookieClick()
-    
+        
+        cookie=0
 
 
-main()
 
-"""cookie=0
+
+        def cookieClick():
+    k=[autocursor(), Grandmother(), Farm()]
+    location = p.locateOnScreen('assets/cok.png')
+    p.moveTo(location)
+    j=0
+    while True: 
+        p.moveTo(location)
+        p.click()
+        if p.pixelMatchesColor(1220,391,(255,255,255)):
+            Farm()
+        elif p.pixelMatchesColor(1218,333,(255,255,255)):
+            Grandmother()
+        elif p.pixelMatchesColor(1218,277,(255,255,255)):
+            autocursor()
+
+
+
     green = p.pixelMatchesColor(1234,294)
     location = p.locateOnScreen('assets/cok.png', confidence = 0.8)
     if location!=None:
